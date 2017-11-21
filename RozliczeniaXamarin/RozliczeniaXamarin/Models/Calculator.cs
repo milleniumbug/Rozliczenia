14cd817067ee88cdb4a86d0c6ed3f609f3679b9b
milleniumbug <milleniumbug@protonmail.com>
2017-11-21 20:23:52 +0100

    Handle empty inputs

--- RozliczeniaXamarin/RozliczeniaXamarin/Models/Calculator.cs
+++ RozliczeniaXamarin/RozliczeniaXamarin/Models/Calculator.cs
@@ -6,35 +6,38 @@ using RozliczeniaXamarin.Models;
 namespace RozliczeniaXamarin
 {
 	public class Calculator
-    {
-	    public static IEnumerable<Transfer> DistributeCosts(IReadOnlyCollection<Payment> initialPayments)
-	    {
-		    var sum = initialPayments.Sum(payment => payment.MoneyAmount);
-		    var avg = sum / initialPayments.Count;
-		    var paidTooMuch = initialPayments.Where(payment => payment.MoneyAmount > avg).ToList();
-		    var paidTooLittle = initialPayments.Where(payment => payment.MoneyAmount < avg).ToList();
-		    var result = new List<Transfer>();
-		    var paidTooMuchIndex = 0;
-		    foreach(var payment in paidTooLittle)
-		    {
-			    var needsToPay = avg - payment.MoneyAmount;
-			    while(needsToPay > 0.0m)
-			    {
-				    var paymentAmount = Math.Min(needsToPay, paidTooMuch[paidTooMuchIndex].MoneyAmount - avg);
-				    if(paymentAmount == 0.0m)
+	{
+		public static IEnumerable<Transfer> DistributeCosts(IReadOnlyCollection<Payment> initialPayments)
+		{
+			if(initialPayments.Count == 0)
+				return Enumerable.Empty<Transfer>();
+
+			var sum = initialPayments.Sum(payment => payment.MoneyAmount);
+			var avg = sum / initialPayments.Count;
+			var paidTooMuch = initialPayments.Where(payment => payment.MoneyAmount > avg).ToList();
+			var paidTooLittle = initialPayments.Where(payment => payment.MoneyAmount < avg).ToList();
+			var result = new List<Transfer>();
+			var paidTooMuchIndex = 0;
+			foreach(var payment in paidTooLittle)
+			{
+				var needsToPay = avg - payment.MoneyAmount;
+				while(needsToPay > 0.0m)
+				{
+					var paymentAmount = Math.Min(needsToPay, paidTooMuch[paidTooMuchIndex].MoneyAmount - avg);
+					if(paymentAmount == 0.0m)
 						break;
-				    needsToPay -= paymentAmount;
-				    paidTooMuch[paidTooMuchIndex] = paidTooMuch[paidTooMuchIndex].Clone(moneyAmount: paidTooMuch[paidTooMuchIndex].MoneyAmount - paymentAmount);
+					needsToPay -= paymentAmount;
+					paidTooMuch[paidTooMuchIndex] = paidTooMuch[paidTooMuchIndex].Clone(moneyAmount: paidTooMuch[paidTooMuchIndex].MoneyAmount - paymentAmount);
 					result.Add(new Transfer(
 						from: payment.Who,
 						to: paidTooMuch[paidTooMuchIndex].Who,
 						moneyAmount: paymentAmount
 					));
-				    if(paidTooMuch[paidTooMuchIndex].MoneyAmount == 0.0m)
-					    paidTooMuchIndex++;
-			    }
-		    }
-		    return result.Select(transfer => transfer.Clone(moneyAmount: Math.Round(transfer.MoneyAmount, 2)));
-	    }
-    }
+					if(paidTooMuch[paidTooMuchIndex].MoneyAmount == 0.0m)
+						paidTooMuchIndex++;
+				}
+			}
+			return result.Select(transfer => transfer.Clone(moneyAmount: Math.Round(transfer.MoneyAmount, 2)));
+		}
+	}
 }
